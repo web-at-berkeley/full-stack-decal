1cbf1472765bfc8dfb77e7b564386a9c9f18d8d0
Arushi Somani <54224195+somaniarushi@users.noreply.github.com>
2021-08-03 13:06:30 -0700

    adding react 2 lecture starter files

---
+++ lectures/react-2/src/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Square() {
+  return (
+    <button className="square">
+      {/* TODO */}
+    </button>
+  );
+}
+
+function Board() {
+const renderSquare = function(i) {
+  return <Square />;
+}
+ 
+const status = 'Next player: X';
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+}
+
+function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
+}
+
+// ========================================
+
+ReactDOM.render(
+<Game />,
+document.getElementById('root')
+);
